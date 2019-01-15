4beae204c8c11d33bc5dd2f97343064d49cfe179
Leocaliban <leocaliban@gmail.com>
2019-01-15 18:31:26 -0300

    Metronotification

---
+++ 03_plugins/01_metro_notification/6.Teste/lib/curso.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){ 
+
+    $.smallBox({
+        title: "Olá, Leonardo!",
+        content: "Bem vindo ao sistema!",
+        fa: "fa-star-o",
+        color: "#259989",
+        timeout: 3000,
+        sound:false,
+        buttonhover: "#0E116E",
+    });
+
+
+});
